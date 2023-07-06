a18e217cda9799a638718ff804662ef2994d18b8
Negin Zahedi <zahedi.negin@gmail.com>
2023-07-06 17:51:33 -0400

    Initial Commit

---
+++ NavigationStack/NavigationStackApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationStackApp.swift
+//  NavigationStack
+//
+//  Created by Negin Zahedi on 2023-07-06.
+//
+
+import SwiftUI
+
+@main
+struct NavigationStackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
